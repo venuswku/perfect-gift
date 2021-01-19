3073e0031c03a8474a4c2c8c3013d72b391bd6b5
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-01-18 19:01:19 -0800

    Enable multiple pages using react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,22 +2,27 @@ import logo from './logo.svg';
 import giveGift from './homepage_image.JPG';
 import React from 'react';
 import './App.css';
+
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+const App = () => (
+  <div className='app'>
+    <h1>React Router Demo</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
+
+
 // import { NavLink, Switch, Route } from 'react-router-dom'
-function App() {
+function Home() {
   return (
     <div className="App">
       <header className="App-header">
         {/* for the top buttons */}
         <div>
           <div class="split right">
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn More
-            </a>
+            <p>Mission</p>
             <p>About Us</p>
             <p>Sign In</p>
           </div>
@@ -36,7 +41,6 @@ function App() {
           {/* dat pic */} 
           <div class="split right">
             <div class="centered">
-              <img src={logo} className="App-logo" alt="logo" />
               <img src={giveGift} alt="give a Gift" />
               {/* <img src="https://www.seekpng.com/ipng/u2t4i1o0w7u2u2q8_logo-gift-giving-icon-png/" alt="Give a Gift"></img> */}
               {/* <a href="https://www.seekpng.com/ipng/u2t4i1o0w7u2u2q8_logo-gift-giving-icon-png/" target="_blank" rel="noreferrer">Logo - Gift Giving Icon Png @seekpng.com</a> */}
@@ -66,5 +70,46 @@ function App() {
   );
 }
 
+function About() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>About Me</h1>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p>
+      <p>Ipsum dolor dolorem consectetur est velit fugiat. Dolorem provident corporis fuga saepe distinctio ipsam? Et quos harum excepturi dolorum molestias?</p> 
+      </header>
+    </div>
+  );
+}
+
+
+function Contact() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Contact Me</h1>
+      <p>You can reach me via email: <strong>hello@example.com</strong></p>
+      </header>
+    </div>
+  );
+}
+
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/about'>About</NavLink></li>
+      <li><NavLink to='/contact'>Contact</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/about' component={About}></Route>
+    <Route exact path='/contact' component={Contact}></Route>
+  </Switch>
+);
 
 export default App;
