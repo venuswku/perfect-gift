ec9a000bec1fddafb91ea937c5e94c3f72efe9ff
klee128 <klee128@ucsc.edu>
2021-01-21 12:49:06 -0800

    add form. presses submit chages url

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -13,12 +13,23 @@ function Login() {
                     <Link to="/mission"><p className="row">Mission</p> {' '}</Link>
                     <Link to="/about"><p className="row">About Us</p>{' '}</Link>
                     <Link to="/login"><p className="row">Sign In</p>{' '}</Link>
+                    <div class="centered">
+                        {/* <h1>Sign In</h1>
+                        <p>Get personalized gift suggestions and share your own gift wishlist!</p> */}
+                        <form>
+                            <label for='username'>Username:</label> <br></br>
+                            <input type='text' name='username'></input> <br></br>
+                            <label for='password'>Password:</label> <br></br>
+                            <input type='text' name='password'></input> <br></br> <br></br>
+                            <input type='submit' value='Submit'></input> <br></br> 
+                        </form>
+                    </div>
+                    
                 </div>
                 <div class="split left">
                     <img src={loginImage} alt="connect with others" className='giveGiftPic' />
                 </div>
-                <h1>Sign In</h1>
-                <p>Get personalized gift suggestions and share your own gift wishlist!</p>
+                
             </header>
         </div>
     );
