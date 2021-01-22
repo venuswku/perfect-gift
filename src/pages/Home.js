a6cdb9012d642f2002bc1da226bc2ca0a60c7551
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-01-21 17:23:04 -0800

    'Get Started' button links to login page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -31,29 +31,16 @@ function Home() {
                             {/* <p className="blueText">Simple and Swift, we are</p>
               <strong className="blueText">Perfect Gift</strong> */}
                             <body>Surprise your family, friends, and loved ones with ideal gifts from their wishlist.</body>
-                            <button type="button">Get Started!</button>
+                            <Link to="/login"><button type="button">Get Started!</button></Link>
+
                         </div>
                     </div>
                 </div>
-
-                {/* everything below is from example */}
-                {/* 
-          <p>
-            Edit <code>src/App.js</code> and save to reload. Test: Sean was here. Can updaet by re
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
-                {/* end of example */}
             </header>
 
         </div>
     );
 }
+
 
 export default Home;
