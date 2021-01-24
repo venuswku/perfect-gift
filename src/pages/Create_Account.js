a30f06450d3cac594ec7ddb4dead5aac1ccf8167
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-01-24 13:34:59 -0800

    add create_account image and basic fields

--- src/pages/Create_Account.js
+++ src/pages/Create_Account.js
@@ -1,55 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../App.css';
-import './Login.css';
-import loginImage from '../images/login_image.png';
+import './Create_Account.css';
+import createAccountImage from '../images/create_account_image.png';
 import PerfectGiftLogo from '../images/website_logo.svg';
 
 
-function Login() {
+function Create_Account() {
     return (
         <div className="App">
-            <header className="App-header">
-                {/* right half of the screen */}
+            <header className="create_account-header">
+                {/* right half of the screen */}     
                 <div class="split right">
                     <p className="row"><Link to="/mission">Mission &nbsp; &nbsp;</Link></p>
                     <p className="row"><Link to="/about">About Us &nbsp; &nbsp;</Link></p>
                     <p className="row"><Link to="/login">Sign In &nbsp; &nbsp;</Link></p>
-                    <div class="centered">
-                        <div class="box">
-                            <h2 class="blueText">CREATE ACCOUNT</h2>
-                            <h5>Get personalized gift suggestions and share your own gift wishlist!</h5>
-                            
-                                <form>
-                                    <label for='username' class='loginFont'>Username:</label>
-                                    <input type='text' name='username' class='loginTextbox'></input>
-                                    <label for='password' class='loginFont'>Password:</label>
-                                    <input type='text' name='password' class='loginTextbox'></input>
-                                    <br/>
-                                    new user? &emsp;
-                                    <input type='submit' value='Submit'></input>
-                                </form>
 
-
-
-
-                        </div>
-
-                    </div>
                 </div>
-                {/* logo + login image */}
 
                 <div class="split left">
-                    {/* <PerfectGiftLogo className="logo" /> */}
                     <Link to="/"><img src={PerfectGiftLogo} alt="Logo" /></Link>
-                    <div class="centered">
-                        <img src={loginImage} alt="connect with others" className='loginPic' />
-                    </div>
                 </div>
+                
+                <div class='centered'>
+                    <p className="blueText">Create an Account </p>
+                    <img src={createAccountImage} alt="the gifters" className='createAccountPic' />
+                    
+                    <form>
+                        <label for='first_name' class='loginFont'>First Name</label>
+                        <input type='text' name='first_name' class='loginTextbox'></input>
+                        <br />
+                        <label for='last_name' class='loginFont'>Last Name</label>
+                        <input type='text' name='last_name' class='loginTextbox'></input>
+                        <br/>
+                        <label for='username' class='loginFont'>Username</label>
+                        <input type='text' name='username' class='loginTextbox'></input>
+                        <br/>
+                        <label for='password' class='loginFont'>Password</label>
+                        <input type='text' name='password' class='loginTextbox'></input>
+                        <br/>
+                        <label for='verify_password' class='loginFont'>Retype Password</label>
+                        <input type='text' name='verify_password' class='loginTextbox'></input>
+                        <br/>
+                        <input type='submit' value='Submit'></input>
+                    </form>
+                </div>
+
+
 
             </header>
         </div>
     );
 }
 
-export default Login;
+export default Create_Account;
