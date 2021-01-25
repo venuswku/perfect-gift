58ce0e0e9a025a004753536f51619882b2d187ce
Claire Ruffing <cmruffin@ucsc.edu>
2021-01-24 17:38:33 -0800

    Adding sql to backend

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,8 +1,8 @@
--- Database schema goes here --
-DROP TABLE IF EXISTS giftuser;
 -- Dummy table --
 DROP TABLE IF EXISTS dummy;
 CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
+-- Database schema goes here --
+DROP TABLE IF EXISTS giftuser;
 -- Your database schema goes here --
 CREATE TABLE giftuser(firstname VARCHAR(32), lastname VARCHAR(32), useremail VARCHAR(32), username VARCHAR(32), userpassword VARCHAR(60), avatar TEXT, showavatar BOOLEAN);
