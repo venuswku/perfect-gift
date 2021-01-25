f5b5e8c8dd83bbe0af381836bae128e53110a808
Venus Ku <venuswku@gmail.com>
2021-01-24 20:21:49 -0800

    add home component to app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Landing from "./pages/Landing"
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -10,21 +11,16 @@ import Create_Account from "./pages/Create_Account";
 
 const App = () => (
   <div className='app'>
-    <h1>React Router Demo</h1>
-    <Main />
+    <Switch>
+      <Route exact path='/' component={Landing}></Route>
+      <Route exact path='/mission' component={Mission}></Route>
+      <Route exact path='/about' component={About}></Route>
+      <Route exact path='/login' component={Login}></Route>
+      <Route exact path='/home' component={Home}></Route>
+      <Route exact path='/profile' component={Profile}></Route>
+      <Route exact path='/create_account' component={Create_Account}></Route>
+    </Switch>
   </div>
-);
-
-//give each page a url
-const Main = () => (
-  <Switch>
-    <Route exact path='/' component={Home}></Route>
-    <Route exact path='/mission' component={Mission}></Route>
-    <Route exact path='/about' component={About}></Route>
-    <Route exact path='/login' component={Login}></Route>
-    <Route exact path='/profile' component={Profile}></Route>
-    <Route exact path='/create_account' component={Create_Account}></Route>
-  </Switch>
 );
 
 export default App;
