c2cd69624c36d85cd9bea32907dabe891b14c3dd
Carlos <cviche@ucsc.edu>
2021-01-24 23:44:19 -0800

    I have added/formatted the text and search bar to the home page (Venus added the navbar. Thanks for that :)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './Home.css';
 import Navbar from '../../navigation/HomeNavbar/HomeNavbar';
+import HomeImage from '../../images/create_account_image.png';
+
+
+
 
 function Home() {
     return (
-        <div className="home">
+        <div className="App">
             <Navbar />
+            <div className="home">
+            </div>
+            <div className="greeting">
+                <p className = "greeting">
+                    Hello, Marvin!
+                </p>
+                <p className="greeting dg">
+                    What gifts are you looking for today?
+                </p>
+            <img src ={HomeImage} alt="the gifters" className="homePic"/>
+            <p className='search-background'>Rounded rectangle</p>
+            <p className='search-background inner'>Search for username</p>
+            </div>
         </div>
     );
 }
