928eb3decde342fb6def44fa37defc274b3796aa
Carlos <cviche@ucsc.edu>
2021-01-26 13:14:35 -0800

    Added search bar/WIP Dropdown

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,27 +1,55 @@
 import React from 'react';
+import '../../App.css';
 import './Home.css';
 import Navbar from '../../navigation/HomeNavbar/HomeNavbar';
 import HomeImage from '../../images/create_account_image.png';
+import { ReactComponent as MagnifyGlass } from '../../images/magnify_glass_grey.svg';
 
 
+// This contains the greeting and image
+function Greeting() {
+    return (
+        <div>
+            <p className = "greeting">
+                    Hello, Marvin!
+                </p>
 
+                <p className="greeting dg">
+                    What gifts are you looking for today?
+                </p>
+
+                <img src ={HomeImage} alt="the gifters" className="homePic"/>          
+        </div>
+    );
+}
+
+function SearchBar () {
+    return (
+        <div className="search-bar">  
+                    
+        <p className='search-background'></p>
+        <MagnifyGlass className="search-background mag"/>
+        {/* <p className='search-background inner center-text'>Search for username</p> */}
+        {/*<p className='search-background inner centered'>Search for username</p> */}
+        <input type="search" className='search-background inner centered' placeholder="Search by username"></input>
+        <p className='search-background inner drop-down'>Search by...</p>
+        <button className="search-background inner drop-down search-button click"></button>
+
+    </div>
+    );
+}
 
 function Home() {
     return (
         <div className="App">
             <Navbar />
+            
             <div className="home">
             </div>
+
             <div className="greeting">
-                <p className = "greeting">
-                    Hello, Marvin!
-                </p>
-                <p className="greeting dg">
-                    What gifts are you looking for today?
-                </p>
-            <img src ={HomeImage} alt="the gifters" className="homePic"/>
-            <p className='search-background'>Rounded rectangle</p>
-            <p className='search-background inner'>Search for username</p>
+                    <Greeting />
+                    <SearchBar />
             </div>
         </div>
     );
