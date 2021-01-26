5d62597ac70f3fc106f34c91341054193ae476e8
klee128 <klee128@ucsc.edu>
2021-01-26 02:30:44 -0800

    change formatting

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -4,6 +4,7 @@ import './Profile.css';
 import Navbar from '../../navigation/HomeNavbar/HomeNavbar';
 import { ReactComponent as EditButton } from '../../images/edit_button.svg';
 import { ReactComponent as DeleteButton } from '../../images/delete_button.svg';
+import { ReactComponent as AddButton } from '../../images/add_button.svg';
 import { ReactComponent as ProfilePic } from '../../images/profile_pic.svg';
 
 
@@ -11,39 +12,51 @@ function Profile() {
     return (
         <div className="App">
             <Navbar/>
-            <div className="App-header">
-                <div className='header-image'><ProfilePic/> </div>
-                
-                <br /> <br /> <br /> <br /> <br /> <br /> 
-                
-                <h3>Marvin Steward</h3>
-                <br></br>
-                <div class='offset'>
-                    <div>
-                        <span class='topicFont'>Username &nbsp;</span>
-                        <span>marvinS &nbsp;</span>
-                        <EditButton />
-                    </div>
-                    <br></br>
-                    {/* replace words with rounded rectangle */}
-                    <div>
-                        <span class='topicFont'>Interests  &nbsp;</span>
-                        <span class='textBubble teal'>topic1 &nbsp; <DeleteButton /></span> 
-                        <span class='textBubble orange'>topic2 &nbsp; <DeleteButton /></span>
-                        <span class='textBubble red'>topic3 &nbsp; <DeleteButton /></span>
-                    </div>
-                    <br></br>
-                    {/* prob do a list instead. <ui>? */}
-                    <div>
-                        <span class='topicFont'>Wishlist &nbsp;  </span>
-                        <span class='textBubble red'>hockey stick &nbsp; <DeleteButton /></span>
-                        <span class='textBubble purple'>hockey shin guard &nbsp; <DeleteButton /></span>
-                        <span class='textBubble green'>Harry Potter wand &nbsp; <DeleteButton /></span>
-                        
-                    </div>  
+            <header class='profile-header'>
+                {/* profile background + pic */}
+                <div class='profile-background'>
+                    <ProfilePic/>
                 </div>
-               
-            </div>
+                <div class='profile-info'>
+                    <h3 class='profile-center'>Marvin Steep</h3>
+                    <br />
+                    {/* username */}
+                    <div>
+                        <span class='topicFont'>Username &nbsp; </span>
+                        <span>msteep</span>
+                        &nbsp;
+                        <EditButton/>
+                    </div>
+                    <br />
+                    {/* interests */}
+                    <div>
+                        <span class='topicFont'>Interests &nbsp; </span>
+                        <span class='textBubble purple'>topic1 &nbsp; <DeleteButton /></span>
+                        <span class='textBubble green'>topic2 &nbsp; <DeleteButton /></span>
+                        <span class='textBubble red'>topic3 &nbsp; <DeleteButton /></span>
+                        <span class='textBubble yellow'>topic4 &nbsp; <DeleteButton /></span>
+                        <span class='textBubble teal'>topic5 &nbsp; <DeleteButton /></span>
+                    </div>
+                    <br />
+                    {/* wishlist */}
+                    <div>
+                        <span class='topicFont'>Wishlist</span>
+                        <ul class='tab no-bullets'>
+                            <li>hockey stick &nbsp; <DeleteButton /></li>
+                            <li>hockey shin guard &nbsp; <DeleteButton /></li>
+                            <li>Harry Potter wand &nbsp; <DeleteButton /></li>
+                            <li>hockey stick &nbsp; <DeleteButton /></li>
+                            <li>hockey shin guard &nbsp; <DeleteButton /></li>
+                            <li>Harry Potter wand &nbsp; <DeleteButton /></li>
+                            <li>hockey stick &nbsp; <DeleteButton /></li>
+                            <li>hockey shin guard &nbsp; <DeleteButton /></li>
+                            <li>Harry Potter wand &nbsp; <DeleteButton /></li>
+                        </ul>
+                        <span class='tab'> <AddButton /> Add to wishlist</span>
+                    </div>
+                    
+                </div>
+            </header>
         </div>
         
     );
