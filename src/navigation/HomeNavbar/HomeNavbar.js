b30d38df616cf95a442ec199cc9100f0f22df4e9
Venus Ku <venuswku@gmail.com>
2021-01-26 15:53:35 -0800

    place in same position for home and logo navbars

--- src/navigation/HomeNavbar/HomeNavbar.js
+++ src/navigation/HomeNavbar/HomeNavbar.js
@@ -7,11 +7,11 @@ import { ReactComponent as MagnifyGlass } from '../../images/magnify_glass.svg';
 function HomeNavbar() {
     return (
         <div className="home-navbar">
-            <Link exact to="/home"><PerfectGiftLogo className="home-navbar-logo" /></Link>
-            <div className="links">
-                <p><Link exact to="/home"><MagnifyGlass/> Find Gift</Link></p>
-                <p><Link exact to="/profile">My Profile</Link></p>
-                <p><Link exact to="/">Sign Out</Link></p>
+            <Link exact to="/home" className="home-navbar-logo"><PerfectGiftLogo/></Link>
+            <div className="navigation-links">
+                <Link exact to="/home" className="link"><MagnifyGlass/> Find Gift</Link>
+                <Link exact to="/profile" className="link">My Profile</Link>
+                <Link exact to="/" className="link">Sign Out</Link>
             </div>
         </div>
     );
