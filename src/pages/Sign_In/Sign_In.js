753880369f7b6ff417a0141d88a6d26cbd6b33fd
Venus Ku <venuswku@gmail.com>
2021-01-28 14:54:06 -0800

    used flexbox for sign in page & matched code to design

--- src/pages/Sign_In/Sign_In.js
+++ src/pages/Sign_In/Sign_In.js
@@ -4,46 +4,37 @@ import './Sign_In.css';
 import Navbar from '../../navigation/LogoNavbar/LogoNavbar';
 import SignInImage from '../../images/sign_in_image.png';
 
-function Login() {
+function Sign_In() {
     return (
-        <div className="App">
+        <div className="SignIn">
             <Navbar/>
-            <header className="App-header">
-                {/* right half of the screen */}
-                <div class="split right">
-                    <div class="centered">
-                        <div class="box">
-                            <h2 class="blueText">Sign In</h2>
-                            <h5>Get personalized gift suggestions and share your own gift wishlist!</h5>
-                            
-                                <form>
-                                    <label for='username' class='loginFont'>Username:</label>
-                                    <input type='text' name='username' class='loginTextbox'></input>
-                                    <label for='password' class='loginFont'>Password:</label>
-                                    <input type='text' name='password' class='loginTextbox'></input>
-                                    <br/>
-                                    <Link to="/create_account">New User? &emsp;</Link>
-                                    <input type='submit' value='Submit'></input>
-                                </form>
-
-
-
-
+            <div class="signInContent">
+                <img src={SignInImage} className="signInPic" alt="connect with others" />
+                <div class="signInForm">
+                    <p class="signInTitle">Sign In</p>
+                    <p class="signInBenefits">Get personalized gift suggestions and share your own gift wishlist!</p>
+                    <form>
+                        <div className="signInField">
+                            <label for='username' class='signInLabel'>Username</label>
+                            <input type='text' name='username' class='signInTextbox'></input>
                         </div>
-
-                    </div>
+                        <div className="signInField">
+                            <label for='password' class='signInLabel'>Password</label>
+                            <input type='text' name='password' class='signInTextbox'></input>
+                        </div>
+                        <br/>
+                        <div className="createAccountOrContinue">
+                            <div className="firstTime">
+                                Is this your first time?
+                                <Link to="/create_account" className="create">Create an account</Link>
+                            </div>
+                            <input type='submit' value='Continue' class='continue'></input>
+                        </div>
+                    </form>
                 </div>
-                {/* logo + login image */}
-
-                <div class="split left">
-                    <div class="centered">
-                        <img src={SignInImage} alt="connect with others" className='signInPic' />
-                    </div>
-                </div>
-
-            </header>
+            </div>
         </div>
     );
 }
 
-export default Login;
+export default Sign_In;
