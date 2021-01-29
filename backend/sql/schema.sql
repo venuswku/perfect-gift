74ee946190ab8411286d64bde0a644db732ae5ae
Venus Ku <venuswku@gmail.com>
2021-01-29 11:26:55 -0800

    defined questionnaireresponses data structure for backend

--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -6,3 +6,19 @@ CREATE TABLE dummy(created TIMESTAMP WITH TIME ZONE);
 
 -- Your database schema goes here --
 CREATE TABLE giftuser(firstname VARCHAR(32), lastname VARCHAR(32), useremail VARCHAR(32), username VARCHAR(32), userpassword VARCHAR(60), avatar TEXT, showavatar BOOLEAN);
+
+CREATE TABLE questionnaireresponses(
+    username VARCHAR(32),
+    outdooractivity VARCHAR(50),
+    place VARCHAR(50),
+    store VARCHAR(50),
+    musicgenre VARCHAR(50),
+    musician VARCHAR(50),
+    band VARCHAR(50),
+    indooractivity VARCHAR(50),
+    movietvshow VARCHAR(50),
+    videogame VARCHAR(50),
+    sport VARCHAR(50),
+    sportteam VARCHAR(50),
+    exercise VARCHAR(50),
+);
