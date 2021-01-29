0a9e1be9532bf8a5ff7a5772cb40dd654c9ab645
Venus Ku <venuswku@gmail.com>
2021-01-28 18:44:29 -0800

    centered content for sign in page

--- src/pages/Sign_In/Sign_In.js
+++ src/pages/Sign_In/Sign_In.js
@@ -8,23 +8,23 @@ function Sign_In() {
     return (
         <div className="SignIn">
             <Navbar/>
-            <div class="signInContent">
+            <div className="signInContent">
                 <img src={SignInImage} className="signInPic" alt="connect with others" />
-                <div class="signInForm">
-                    <p class="signInTitle">Sign In</p>
-                    <p class="signInBenefits">Get personalized gift suggestions and share your own gift wishlist!</p>
+                <div className="signInForm">
+                    <p className="signInTitle">Sign In</p>
+                    <p className="signInBenefits">Get personalized gift suggestions and share your own gift wishlist!</p>
                     <form className="userInfo">
-                        <label for='username' class='signInLabel'>Username</label>
-                        <input type='text' name='username' class='signInTextbox'></input>
-                        <label for='password' class='signInLabel'>Password</label>
-                        <input type='text' name='password' class='signInTextbox'></input>
+                        <label for='username' className='signInLabel'>Username</label>
+                        <input type='text' name='username' className='signInTextbox'></input>
+                        <label for='password' className='signInLabel'>Password</label>
+                        <input type='text' name='password' className='signInTextbox'></input>
                         <br/>
                         <div className="createAccountOrContinue">
                             <div className="firstTime">
                                 Is this your first time?
                                 <Link to="/create_account" className="create">Create an account</Link>
                             </div>
-                            <input type='submit' value='Continue' class='continue'></input>
+                            <input type='submit' value='Continue' className='continue'></input>
                         </div>
                     </form>
                 </div>
