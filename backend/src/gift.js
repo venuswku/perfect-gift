c2d3009026eeadaaaf872bcfae4bf8a7c2a14236
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-02-01 14:47:46 -0800

    Enable hashing on gift.js

--- backend/src/gift.js
+++ backend/src/gift.js
@@ -1,4 +1,6 @@
 const db = require('./db');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 // gets a single user or all of the users from the giftusers table
 exports.getUsers = async (req, res) => {
@@ -21,16 +23,25 @@ exports.login = async (req, res) => {
     console.log("We are going to authenticate the request that the frontend has given us")
     console.log("The frontend has given us:")
     console.log(req.body.username, req.body.password)
+    var oneUser = false
+    //res.status(200).send()
 
     // If a username is passed into query param (name of query is username, in openapi.yaml)
     if (req.body.username) {
         console.log("Entered IF")
         // Get the single user's data if the user is selected
-        const oneUser = await db.authenticateUser(req.body.username);
+        await bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+            oneUser = await db.authenticateUser(req.body.username, hash);
+            console.log(hash)
+            //console.log(oneUser)
+        })
         console.log("Exited DB function")
         if (oneUser) {
+            console.log("One User True")
             res.status(200).json(oneUser);
         } else {
+            console.log("One User False")
+            console.log(oneUser)
             res.status(404).send();
         }
     }
