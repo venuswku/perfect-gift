b093c251d980de6853b5b077a8f74075d74dacd4
klee128 <klee128@ucsc.edu>
2021-02-01 17:44:34 -0800

    save

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -7,10 +7,10 @@ INSERT INTO giftuser(username, userpassword, firstname, lastname, useremail, ava
 INSERT INTO giftuser(username, userpassword, firstname, lastname, useremail, avatar, showavatar) VALUES ('sobyrne', '', 'Sean', 'OByrne', 'sean234@mail.com', '', FALSE);
 INSERT INTO giftuser(username, userpassword, firstname, lastname, useremail, avatar, showavatar) VALUES ('klee', '', 'Kelsy', 'Lee', 'kelsylee@mail.com', '', FALSE);
 
-DELETE FROM questionnaireresponses;
-INSERT INTO questionnaireresponses(
-    username, outdooractivity, place, store, musicgenre, musician,band,
-    indooractivity, movietvshow, videogame, sport, sportteam, exercise)
-    VALUES ('klee128', 'walk', '', '', 'pop', '', '', 'sleeping', 'True Beauty', 'Tetris', '', '', 'walk');
+-- DELETE FROM questionnaireresponses;
+-- INSERT INTO questionnaireresponses(
+--     username, outdooractivity, place, store, musicgenre, musician,band,
+--     indooractivity, movietvshow, videogame, sport, sportteam, exercise)
+--     VALUES ('klee128', 'walk', '', '', 'pop', '', '', 'sleeping', 'True Beauty', 'Tetris', '', '', 'walk');
 
     
