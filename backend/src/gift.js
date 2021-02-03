117b3960881a3736b790ec4cd9c10538b3635662
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-02-02 17:02:04 -0800

    Fix login typo

--- backend/src/gift.js
+++ backend/src/gift.js
@@ -34,30 +34,30 @@ exports.getQResponse = async (req, res) => {
     //if no info found, sends []
 };
 
-    exports.login = async (req, res) => {
-        console.log("We are going to authenticate the request that the frontend has given us")
-        console.log("The frontend has given us:")
-        console.log(req.body.username, req.body.password)
-        var oneUser = false
-        //res.status(200).send()
+exports.login = async (req, res) => {
+    console.log("We are going to authenticate the request that the frontend has given us")
+    console.log("The frontend has given us:")
+    console.log(req.body.username, req.body.password)
+    var oneUser = false
+    //res.status(200).send()
 
-        // If a username is passed into query param (name of query is username, in openapi.yaml)
-        if (req.body.username) {
-            console.log("Entered IF")
-            // Get the single user's data if the user is selected
-            await bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
-                oneUser = await db.authenticateUser(req.body.username, hash);
-                console.log(hash)
-                //console.log(oneUser)
-            })
-            console.log("Exited DB function")
-            if (oneUser) {
-                console.log("One User True")
-                res.status(200).json(oneUser);
-            } else {
-                console.log("One User False")
-                console.log(oneUser)
-                res.status(404).send();
-            }
+    // If a username is passed into query param (name of query is username, in openapi.yaml)
+    if (req.body.username) {
+        console.log("Entered IF")
+        // Get the single user's data if the user is selected
+        await bcrypt.hash(req.body.password, saltRounds, async (err, hash) => {
+            oneUser = await db.authenticateUser(req.body.username, hash);
+            console.log(hash)
+            //console.log(oneUser)
+        })
+        console.log("Exited DB function")
+        if (oneUser) {
+            console.log("One User True")
+            res.status(200).json(oneUser);
+        } else {
+            console.log("One User False")
+            console.log(oneUser)
+            res.status(404).send();
         }
     }
+};
