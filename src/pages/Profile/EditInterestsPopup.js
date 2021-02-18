5c33fd3a6f636dfea6de666569d326512709db15
Venus Ku <venuswku@gmail.com>
2021-02-18 12:04:39 -0800

    moved all questionnaire states from EditInterestsPopup to Profile (so we don't need to refresh the page to see new changes for questionnaire responses)

--- src/pages/Profile/EditInterestsPopup.js
+++ src/pages/Profile/EditInterestsPopup.js
@@ -20,81 +20,39 @@ class EditQuestionnaireResponsesPopup extends Component {
     
     constructor(props) {
         super(props)
-        // items that we will be able to send to the server
-        this.state = {
-            username: this.props.userInfo.username,
-            outdooractivity: '',
-            place: '',
-            store: '',
-            musicgenre: '',
-            musician: '',
-            band: '',
-            indooractivity: '',
-            movietvshow: '',
-            videogame: '',
-            sport: '',
-            sportsteam: '',
-            exercise: '',
-        }
+        /* Parent component (Profile.js) passed the following properties through this.props:
+            - toggle: makes popup open/close
+            - userInfo: contains username, questionnainre responses, etc.
+            - editInterest: handles changes to any state
+        */
     }
 
-    // load user's original questionnaire responses
-    componentDidMount() {
-        axios.get(`http://localhost:3010/v0/getqresponse/${this.state.username}`, this.state)
-        .then(res => {
-            this.setState({ outdooractivity: res.data[0].outdooractivity });
-            this.setState({ place: res.data[0].place });
-            this.setState({ store: res.data[0].store });
-            this.setState({ musicgenre: res.data[0].musicgenre });
-            this.setState({ musician: res.data[0].musician });
-            this.setState({ band: res.data[0].band });
-            this.setState({ indooractivity: res.data[0].indooractivity });
-            this.setState({ movietvshow: res.data[0].movietvshow });
-            this.setState({ videogame: res.data[0].videogame });
-            this.setState({ sport: res.data[0].sport });
-            this.setState({ sportsteam: res.data[0].sportsteam });
-            this.setState({ exercise: res.data[0].exercise });
-        }).catch(res => {
-            console.log(res);
-        })
-    }
-
-    // if questionnaire field is changed, update appropriately
-    changeHandler = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    }
-
-    // closes popup
-    closePopup = () => {
-        this.props.toggle();
-    };
+    componentDidMount() { }
 
     // closes popup and saves changes to questionnaire responses
     saveChanges = () => {
         this.props.toggle();
-        axios.put('http://localhost:3010/v0/putqresponse/' + this.state.username, [this.state])
+        axios.put('http://localhost:3010/v0/putqresponse/' + this.props.userInfo.username, [this.props.userInfo])
         .then(response => {
             console.log('EditInterestsPopup.js: success updating qr');
             console.log(response);
-            // refresh profile page to see user changes
-            window.location.reload();
         })
         .catch(error => {
             console.log("EditInterestsPopup.js: failed updating qr");
-            console.log(this.state);
+            console.log(this.props.userInfo);
             console.log(error);
         });
     };
 
     render() {
         // store input values locally into the following values
-        const { outdooractivity, place, store, musicgenre, musician, band, indooractivity, movietvshow, videogame, sport, sportsteam, exercise } = this.state;
+        const { outdooractivity, place, store, musicgenre, musician, band, indooractivity, movietvshow, videogame, sport, sportsteam, exercise } = this.props.userInfo;
 
         return (
             <div className="EditInterestsPopup">
-                <div className="questionnairePopupBackground" onClick={this.closePopup}></div>
+                <div className="questionnairePopupBackground" onClick={this.props.toggle}></div>
                 <div className="questionnairePopupContent">
-                    <span className="close" onClick={this.closePopup}>&times;</span>
+                    <span className="close" onClick={this.props.toggle}>&times;</span>
                     <div className="updateInterestsTitle">Edit your Interests</div>
                     <p className="updateInterestsInstructions">Make sure to save your changes at the bottom of this popup!</p>
                     <table className="questionnaire">
@@ -105,7 +63,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <p className="blueText interestTopic"><img src={indoors1Image} alt="food" />&nbsp;&nbsp;Indoors&nbsp;&nbsp;<img src={indoors2Image} alt="camera" /></p>
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={indooractivity}
                                             name='indooractivity'
                                             className='caTextbox'
@@ -114,7 +72,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={movietvshow}
                                             name='movietvshow'
                                             className='caTextbox'
@@ -123,7 +81,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={videogame}
                                             name='videogame'
                                             className='caTextbox'
@@ -137,7 +95,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <p className="blueText interestTopic"><img src={outdoors1Image} alt="amusement park" />&nbsp;&nbsp;Outdoors&nbsp;&nbsp;<img src={outdoors2Image} alt="tree" /></p>
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={outdooractivity}
                                             name='outdooractivity'
                                             className='caTextbox'
@@ -146,7 +104,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={place}
                                             name='place'
                                             className='caTextbox'
@@ -155,7 +113,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={store}
                                             name='store'
                                             className='caTextbox'
@@ -171,7 +129,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <p className="blueText interestTopic"><img src={sports1Image} alt="weightlifter" />&nbsp;&nbsp;Sports&nbsp;&nbsp;<img src={sports2Image} alt="basketball" /></p>
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={sport}
                                             name='sport'
                                             className='caTextbox'
@@ -180,7 +138,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={sportsteam}
                                             name='sportsteam'
                                             className='caTextbox'
@@ -189,7 +147,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={exercise}
                                             name='exercise'
                                             className='caTextbox'
@@ -203,7 +161,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <p className="blueText interestTopic"><img src={music1Image} alt="keyboard" />&nbsp;&nbsp;Music&nbsp;&nbsp;<img src={music2Image} alt="music note" /></p>
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={musicgenre}
                                             name='musicgenre'
                                             className='caTextbox'
@@ -212,7 +170,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={musician}
                                             name='musician'
                                             className='caTextbox'
@@ -221,7 +179,7 @@ class EditQuestionnaireResponsesPopup extends Component {
                                         <br />
                                         <input
                                             type='text'
-                                            onChange={this.changeHandler}
+                                            onChange={this.props.editInterest}
                                             value={band}
                                             name='band'
                                             className='caTextbox'
