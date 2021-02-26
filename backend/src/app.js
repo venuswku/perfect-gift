1473b21bfd6e5e2cc19af65d2f75eb6d47db9240
klee128 <klee128@ucsc.edu>
2021-02-25 17:26:13 -0800

    fixed the cors errors so backend now works with frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,10 @@ const proxy = require('http-proxy-middleware')
 
 // Used for letting the frontend communicate with the server
 app.use(cors({
-  origin: "https://backend-perfectgift.com",
+  // 
+  origin: "https://aws-frontend.d3i4pwwftbhh87.amplifyapp.com", 
   methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Origin", "X - Requested-With", "Content-Type", "Accept"],
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
   credentials: true,
 }));
 
