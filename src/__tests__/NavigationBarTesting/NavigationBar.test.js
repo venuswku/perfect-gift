0bf2070b31662abf6a03173e3ddb8995d1a5ac1b
Sean O'Byrne <37026236+smobyrne@users.noreply.github.com>
2021-03-02 18:39:24 -0800

    WIP HomeNavBar

--- src/__tests__/NavigationBarTesting/NavigationBar.test.js
+++ src/__tests__/NavigationBarTesting/NavigationBar.test.js
@@ -7,6 +7,7 @@ import {
   screen,
 } from "@testing-library/react";
 import Landing from "../../pages/Landing/Landing";
+import Home from "../../pages/Home/Home";
 import { MemoryRouter } from "react-router-dom";
 
 // Test for Landing Nav Bar
@@ -32,21 +33,27 @@ test("LogoNavBar", () => {
   //   </MemoryRouter>
   // );
 
-  // // Test that Landing NavBar links to Sign In page
+  // Test that Landing NavBar links to Sign In page
   // expect(screen.getByText('Sign In').closest('a')).toHaveAttribute('href', '/sign_in')
 
 });
 
 // Test for Home Nav Bar
 test("HomeNavBar", () => {
-  // Setup Testing Fixture
-  // render(
-  //   <MemoryRouter>
-  //     <Landing />
-  //   </MemoryRouter>
-  // );
+  //Setup Testing Fixture
+  render(
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
+  );
 
-  // // Test that Landing NavBar links to Sign In page
-  // expect(screen.getByText('Sign In').closest('a')).toHaveAttribute('href', '/sign_in')
+  //Test that Home NavBar links to respective pages
+  expect(screen.getByText('Find Gift').closest('a')).toHaveAttribute('href', '/home')
+  expect(screen.getByText('My Profile').closest('a')).toHaveAttribute('href', '/profile')
+  expect(screen.getByText('Sign Out').closest('a')).toHaveAttribute('href', '/')
+
+  //Add Test that Sign Out Calls SignOut FUnction?
+
+  //Add test that clicking logo goes to /home?
 
 });
