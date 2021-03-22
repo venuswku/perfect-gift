2238159b04a10a846eb9def55efa93842cab8500
Venus Ku <venuswku@gmail.com>
2021-03-22 16:25:18 -0700

    simplify backend branch

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app.js');
-const PORT = process.env.PORT || 8080; // use either the host env var port (PORT) provided by Heroku or the local port (3010) on your machine
+const PORT = process.env.PORT || 8080; // use either the host env var port (PORT) provided by Heroku or the local port (8080) on your machine
 
 // Start server and listen on specified port.
 app.listen(PORT, () => {
